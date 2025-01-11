0899ebb937a0b07dc5c83fe3f3092aabc64ced5d
abrarkhalidofficial <iabrarkhalidofficial@gmail.com>
2025-01-11 11:26:07 +0500

    Update page.tsx

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Register from "./Register";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import prisma from "@/lib/prisma";
 
 export default async function RegisterServer() {
   const token = (await cookies()).get("token")?.value;
